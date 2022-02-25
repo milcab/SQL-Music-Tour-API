99f25661f998a3a8e82f71dd21559a82adaede7f
milcab <milcarodriguez23@gmail.com>
2022-02-25 17:09:00 -0500

    adding the packages and the files

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
