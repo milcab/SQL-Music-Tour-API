5c59a642f5b10d79e65f1b8205afafc39c7cf18a
milcab <milcarodriguez23@gmail.com>
2022-02-28 18:33:43 -0500

    setting up recommendation migration

---
+++ migrations/20220228225708-add-recommendation-column-to-bands.js
@@ -0,0 +1,25 @@
+'use strict';
+const {DataTypes} = require('sequelize')
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn ('bands', 'recommendation', {
+      type: DataTypes.STRING
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
