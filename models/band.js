bf771faabfc6b80b8be789f3f047faf1e97e2be6
milcab <milcarodriguez23@gmail.com>
2022-02-28 15:14:49 -0500

    setting up

---
+++ models/band.js
@@ -0,0 +1,51 @@
+// DEPENDENCIES
+const { Sequelize, DataTypes, Model } = require('sequelize')
+const sequelize = new Sequelize(process.env.PG_URI)
+
+// MODEL
+class Band extends Model { }
+
+ClassName.init({
+    column_name: DataTypes.TEXT,
+    column_name_two: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    modelName: 'ModelName',
+    tableName: 'table_name'
+    // extra options here 
+})
+
+Band.init({
+    band_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    available_start_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    end_time: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+}, {
+    sequelize,
+    modelName: 'Band',
+    tableName: 'band',
+    timestamps: false
+}) 
+
+// EXPORT
+module.exports = Band
